df9a54ce26858fcf9b5486107b805a1365dc6d27
Oren <orengolan@gmail.com>
2016-01-10 18:34:44 -0800

    v 1.1

--- dist.sh
+++ dist.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+if [ -d "$DIRECTORY" ]; then
+  rm dist
+fi
 mkdir dist
 cp -r templates carousel.js content-script.js event-page.js icon* key.pem manifest.json dist
 zip -r mm.zip dist
